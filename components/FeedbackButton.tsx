ae550d6660adfe0110942871dd8800a3f2fa37f3
danilito222222 <danilito222222@users.noreply.github.com>
2026-01-14 01:37:38 +0700

    Show vote counts on feedback buttons

--- components/FeedbackButton.tsx
+++ components/FeedbackButton.tsx
@@ -5,10 +5,21 @@ import React, { useState, useEffect } from 'react';
 export function FeedbackButton() {
     const [hasVoted, setHasVoted] = useState<boolean | null>(null);
     const [submitting, setSubmitting] = useState(false);
+    const [votes, setVotes] = useState({ yes: 0, no: 0 });
 
     useEffect(() => {
         const voted = localStorage.getItem('roblox-bypass-voted');
         setHasVoted(voted !== null);
+
+        // Fetch current votes
+        fetch('/api/vote')
+            .then(res => res.json())
+            .then(data => {
+                if (data && typeof data.yes === 'number') {
+                    setVotes(data);
+                }
+            })
+            .catch(console.error);
     }, []);
 
     const handleVote = async (type: 'yes' | 'no') => {
@@ -23,6 +34,8 @@ export function FeedbackButton() {
             });
 
             if (res.ok) {
+                const newVotes = await res.json();
+                setVotes(newVotes);
                 setHasVoted(true);
                 localStorage.setItem('roblox-bypass-voted', type);
             } else if (res.status === 429) {
@@ -70,7 +83,7 @@ export function FeedbackButton() {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                             <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                         </svg>
-                        Да, помог!
+                        Да, помог! ({votes.yes})
                     </button>
                     <button
                         onClick={() => handleVote('no')}
@@ -80,7 +93,7 @@ export function FeedbackButton() {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                             <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
                         </svg>
-                        Нет
+                        Нет ({votes.no})
                     </button>
                 </div>
             </div>
